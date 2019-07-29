05d00116d7324551577ec074875163ccf15ca9a4
Qendolin <32160662+Qendolin@users.noreply.github.com>
2019-07-29 21:39:49 +0200

    Add workspace config

--- dev-setup.bat
+++ dev-setup.bat
@@ -5,4 +5,13 @@ mkdir pokecord-bot
 cd ./pokecord-bot
 git clone https://github.com/Qendolin/pokecord-bot.git .
 
+echo Reccomended VSCode extensions:
+echo.
+echo * Prettier - Code Formatter
+echo * TypeScipt Hero
+echo * TSLint
+echo * GitLens
+echo.
+
 echo Finished
+pause
