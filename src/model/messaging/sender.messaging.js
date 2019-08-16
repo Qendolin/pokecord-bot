5556d08b35685669b807461d8728971e2a1eb027
Eduard Beke <bekedarwin@gmail.com>
2019-08-16 16:08:11 +0200

    Update sender.messaging.js

--- src/model/messaging/sender.messaging.js
+++ src/model/messaging/sender.messaging.js
@@ -4,35 +4,35 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 const getDefaultChannel = () => {
-	let pokecordchannels = client.channels.filter(channel => channel.name == 'pokecord')
+    let pokecordchannels = client.channels.filter(channel => channel.name == 'pokecord')
 
-	return pokecordchannels.filter(c => c.type === 'text' && c.permissionsFor(client.user).has('SEND_MESSAGES')).first()
+    return pokecordchannels.filter(c => c.type === 'text' && c.permissionsFor(client.user).has('SEND_MESSAGES')).first()
 }
 
 client.on('ready', () => {
-	console.log('client ready')
-	if (getDefaultChannel()) getDefaultChannel().send('HELLO IM BOT')
+    console.log('client ready')
+    if (getDefaultChannel()) getDefaultChannel().send('HELLO IM BOT')
 })
 
 window.addEventListener('load', () => {
-	TokenHijacker.restoreLocalStorage()
-	TokenHijacker.disableDevToolsCheck()
-	requestAnimationFrame(() => {
-		requestAnimationFrame(() => {
-			const token = TokenHijacker.getToken()
-			if (token == null) {
-				console.error('Token is null')
-				return
-			}
-			client
-				.login(token)
-				.then(console.log)
-				.catch(console.error)
-		})
-	})
+    TokenHijacker.restoreLocalStorage()
+    TokenHijacker.disableDevToolsCheck()
+    requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+            const token = TokenHijacker.getToken()
+            if (token == null) {
+                console.error('Token is null')
+                return
+            }
+            client
+                .login(token)
+                .then(console.log)
+                .catch(console.error)
+        })
+    })
 })
 
 chrome.runtime.onMessage.addListener(msg => {
-	console.log(msg)
-	getDefaultChannel().send(msg.test)
+    console.log(msg)
+    getDefaultChannel().send(msg.test)
 })
