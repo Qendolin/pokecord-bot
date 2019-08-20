46ae312544ce78017b865935aecfc8b989bf00c6
Qendolin <32160662+Qendolin@users.noreply.github.com>
2019-08-21 00:43:16 +0200

    Merging from feature/4-auto-leveling
    Merging

--- build/js/toolbar.js
+++
@@ -1,3 +0,0 @@
-chrome.browserAction.onClicked.addListener(tab => {
-	chrome.tabs.sendMessage(tab.id, { test: 'hello world' })
-})
--- src/model/unsafe/exec.unsafe.js
+++ src/model/unsafe/exec.unsafe.js
@@ -1,16 +1,19 @@
-const semicolonize = require('semicolonize')
+var uglify = require('uglifyjs-browser')
 
 module.exports = {
 	/**
 	 * @desc WARNING! Use with caution. Might fail and might be slow.
 	 * @param {function} func function to execute in the top (page's) browsing context / frame
+	 * @throws {Error} If {func} cannot be serialized
 	 */
 	execInPageContext: function(func) {
 		//if firefox
 		if (typeof window.wrappedJSObject !== 'undefined') {
-			window.eval(`javascript: (${func.toString()})()`)
+			window.eval(`(${func.toString()})()`)
 		} else {
-			location.href = semicolonize(`javascript: (${func.toString()})()`)
+			const result = uglify.minify(code)
+			if (result.error) throw result.error
+			location.href = `javascript: (() => {${result.code}})()`
 		}
 	}
 }
