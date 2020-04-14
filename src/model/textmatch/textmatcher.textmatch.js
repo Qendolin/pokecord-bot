c56a917ba3bf0bc137348b403265fc355ca72edc
Qendolin <32160662+Qendolin@users.noreply.github.com>
2020-04-14 19:06:55 +0200

    added TextMatcher class

---
+++ src/model/textmatch/textmatcher.textmatch.js
@@ -0,0 +1,14 @@
+const dHash = require('./image/dhash.image.autocatch')
+
+class TextMatcher {
+	constructor(patternText) {}
+
+	/**
+	 * @param {string} text
+	 * @returns {bool} Ture if `text` matches
+	 */
+	async match(text) {
+		//TODO
+		//render the text onto a canvas, then compare to patternText with dHash
+	}
+}
